1cb4846019ddf4d4ffb9eb08c32a5eaa6da8f10b
IbrahimSaad54 <ibrahim.saad.2004e@gmail.com>
2025-11-15 16:35:46 +0200

    Format Document

--- script.js
+++ script.js
@@ -5,54 +5,54 @@ var menuBtn = document.querySelector('.main-navbar .menu-btn');
 var menuList = document.querySelector('.main-navbar .nav-list');
 var menuListItems = document.querySelectorAll('.nav-list li a');
 
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menuList.classList.toggle('active');
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menuList.classList.toggle('active');
 });
 
-for(var i = 0; i < menuListItems.length; i++){
-	menuListItems[i].addEventListener('click', menuItemClicked);
+for (var i = 0; i < menuListItems.length; i++) {
+    menuListItems[i].addEventListener('click', menuItemClicked);
 }
-function menuItemClicked(){
-	menuBtn.classList.remove('active');
-	menuList.classList.remove('active');
+function menuItemClicked() {
+    menuBtn.classList.remove('active');
+    menuList.classList.remove('active');
 }
 
 var homeSection = document.querySelector('.home');
 window.addEventListener('scroll', pageScrollFunction);
 window.addEventListener('load', pageScrollFunction);
 
-function pageScrollFunction(){
-	if(window.scrollY > 120){
-		homeSection.classList.add('active');
-	}
-	else{
-		homeSection.classList.remove('active');
-	}
+function pageScrollFunction() {
+    if (window.scrollY > 120) {
+        homeSection.classList.add('active');
+    }
+    else {
+        homeSection.classList.remove('active');
+    }
 }
 // Home Section Ends
 
 // Partners Section Starts 
 $('.partners-slider').owlCarousel({
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    margin:10,
-    nav:true,
-    navText:["<i class='fa-solid fa-arrow-left'></i>",
-             "<i class='fa-solid fa-arrow-right'></i>"],
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa-solid fa-arrow-left'></i>",
+        "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive: {
+        0: {
+            items: 1
         },
-        500:{
-            items:2
+        500: {
+            items: 2
         },
-        700:{
-            items:3
+        700: {
+            items: 3
         },
-        1000:{
-        	items:5
+        1000: {
+            items: 5
         }
     }
 })
@@ -61,19 +61,19 @@ $('.partners-slider').owlCarousel({
 
 // Testimonials Section Starts
 $('.testimonials-slider').owlCarousel({
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:6000,
-    margin:10,
-    nav:true,
-    navText:["<i class='fa-solid fa-arrow-left'></i>",
-             "<i class='fa-solid fa-arrow-right'></i>"],
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa-solid fa-arrow-left'></i>",
+        "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive: {
+        0: {
+            items: 1
         },
-        768:{
-            items:2
+        768: {
+            items: 2
         }
     }
 })
